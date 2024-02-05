568a9f8e2d0b57df660fe911cd8c797568c110de
雷文勇 <35560582+aierx@users.noreply.github.com>
2024-02-06 00:28:01 +0800

    migrations

---
+++ webapi/Migrations/20240205153424_i6.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    public partial class i6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "suffix",
+                table: "t_resource",
+                newName: "realPath");
+
+            migrationBuilder.RenameColumn(
+                name: "fileName",
+                table: "t_resource",
+                newName: "fileupName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "contentType",
+                table: "t_resource",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "fileExtention",
+                table: "t_resource",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "fileOrginname",
+                table: "t_resource",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "contentType",
+                table: "t_resource");
+
+            migrationBuilder.DropColumn(
+                name: "fileExtention",
+                table: "t_resource");
+
+            migrationBuilder.DropColumn(
+                name: "fileOrginname",
+                table: "t_resource");
+
+            migrationBuilder.RenameColumn(
+                name: "realPath",
+                table: "t_resource",
+                newName: "suffix");
+
+            migrationBuilder.RenameColumn(
+                name: "fileupName",
+                table: "t_resource",
+                newName: "fileName");
+        }
+    }
+}
---
+++ webapi/Migrations/20240205153633_i7.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    public partial class i7 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "realPath",
+                table: "t_resource");
+
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "t_resource");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "realPath",
+                table: "t_resource",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "type",
+                table: "t_resource",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
